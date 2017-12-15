decc13011cf62ab9011482c86e4611513e894959
parasthekoder <khandelwalparas8@gmail.com>
2017-12-15 23:59:11 +0530

    Widget for Alert Button Saveme is added

--- app/src/main/java/com/developers/droidteam/merisafety/MainActivity.java
+++ app/src/main/java/com/developers/droidteam/merisafety/MainActivity.java
@@ -1,21 +1,17 @@
 package com.developers.droidteam.merisafety;
 
-import android.*;
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -31,10 +27,6 @@ import android.widget.ViewFlipper;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ResultCodes;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.SignInButton;
@@ -42,19 +34,14 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -147,7 +134,7 @@ public class MainActivity extends AppCompatActivity  {
                 if (!isGooglePlayServicesAvailable()) {
                     finish();
                 }
-                updateUI(true,true);
+                updateUI(true);
 
 // Create and launch sign-in intent
                 startActivityForResult(
@@ -190,7 +177,7 @@ public class MainActivity extends AppCompatActivity  {
         protected void onPostExecute(Void result) {
             if (dialog.isShowing()) {
                 dialog.dismiss();
-                updateUI(true,false);
+                updateUI(true);
             }
         }
 
@@ -210,26 +197,19 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
-    public void updateUI(Boolean value,Boolean click)
+    public void updateUI(Boolean value)
     {
         if(value)
         {
-            if(click) {
-                signInButton.setVisibility(View.INVISIBLE);
-            }
-            else {
                 signInButton.setVisibility(View.INVISIBLE);
                 progressBar.setVisibility(View.VISIBLE);
-            }
+
         }
         else
         {
-           if(click){
-               signInButton.setVisibility(View.VISIBLE);
-           }else{
                signInButton.setVisibility(View.VISIBLE);
                progressBar.setVisibility(View.INVISIBLE);
-           }
+
         }
 
     }
@@ -312,7 +292,7 @@ public class MainActivity extends AppCompatActivity  {
                 // ...
             } else {
 
-                updateUI(false,true);
+                updateUI(false);
 
                 // Sign in failed, check response for error code
                 // ...
---
+++ app/src/main/java/com/developers/droidteam/merisafety/NewAppWidget.java
@@ -0,0 +1,54 @@
+package com.developers.droidteam.merisafety;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.app_widget_text);
+        Intent intent = new Intent(context, SaveMeActivity.class).putExtra("key", R.id.save_me);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        views.setTextViewText(R.id.save_me_widget, widgetText);
+        views.setOnClickPendingIntent(R.id.save_me_widget, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
+    }
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
